30ea4e74245e93eaa25b3ec278b67034258039de
yalhasan <yalhasan13@gmail.com>
2023-10-01 20:26:00 +0300

    task1+2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import yarns from "./products";
+
+let productList = yarns.map((yarn) => {
+  return (
+    <div className="product">
+      <div className="text">
+        <img src={yarn.img} width="100px" height="100px" />
+        <h2>{yarn.name}</h2>
+        <h4>{yarn.weight}</h4>
+        <h4>{yarn.price}</h4>
+      </div>
+    </div>
+  );
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text">
+      <h1>Yarn shop</h1>
+      <h4>
+        all the yarns you'll need for your knitting, crochetting, tufting and
+        any other related projects
+      </h4>
+      <img
+        src="https://i.etsystatic.com/13197440/r/il/3cc0b3/4220294296/il_fullxfull.4220294296_go99.jpg"
+        width="400px"
+        height="400px"
+      />
+      <div className="product">{productList}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,31 @@
+const yarns = [
+  {
+    name: "wool",
+    price: "2KD",
+    weight: "100g",
+    img: "https://i.etsystatic.com/14957687/r/il/7b45d8/2866761953/il_fullxfull.2866761953_mw3x.jpg",
+  },
+
+  {
+    name: "cotton",
+    price: "3KD",
+    weight: "100g",
+    img: "https://m.media-amazon.com/images/I/71raAoXuDML.jpg",
+  },
+
+  {
+    name: "acrylic",
+    price: "1KD",
+    weight: "100g",
+    img: "https://www.cchobby.com/media/catalog/product/cache/f5a04654fa1291930ec6413dd4db78d6/4/2/421861_1_2.jpg",
+  },
+
+  {
+    name: "velvet",
+    price: "3KD",
+    weight: "100g",
+    img: "https://m.media-amazon.com/images/I/81boLvharjL._AC_UF894,1000_QL80_.jpg",
+  },
+];
+
+export default yarns;
